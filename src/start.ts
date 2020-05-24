08aa7194996a1b9d57f3436657a7ad5b1b7cd420
Pranav Bhat <38520250+PranavBhatS@users.noreply.github.com>
2020-05-24 19:37:37 +0530

    addres

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,5 @@
 import MainServer from './MainServer';
 
 const exampleServer = new MainServer();
-const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
+const { PORT=3000, LOCAL_ADDRESS='157.45.239.175' } = process.env
 exampleServer.start(process.env.NODE_PORT || PORT,LOCAL_ADDRESS);
