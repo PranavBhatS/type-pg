288f919f455ef0106f69e06f9fd7c0d6a66ff468
Pranav Bhat <38520250+PranavBhatS@users.noreply.github.com>
2020-05-24 19:07:39 +0530

    added cors

--- src/MainServer.ts
+++ src/MainServer.ts
@@ -3,13 +3,20 @@ import * as controllers from './controllers';
 import { Server } from '@overnightjs/core';
 import { Logger } from '@overnightjs/logger';
 import { sequelize } from './services/database/db.config';
-
+import * as cors from "cors";
 class MainServer extends Server {
     private readonly SERVER_STARTED = 'main server started on port: ';
     constructor() {
         super(true);
+        const options: cors.CorsOptions = {
+            allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            origin:'https://pgdbtype.herokuapp.com',
+            preflightContinue: false
+        };
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({ extended: true }));
+        this.app.use(cors(options))
         this.setupControllers();
     }
 
@@ -28,14 +35,14 @@ class MainServer extends Server {
             res.send(this.SERVER_STARTED + port);
         });
         this.app.listen(port, () => {
-            sequelize.authenticate().then(async()=>{
+            sequelize.authenticate().then(async () => {
                 await sequelize.sync()
                 Logger.Imp("db connected");
             })
-            .catch(err=>{
-                console.log(err)
-                Logger.Err("error occered during db connection");
-            })
+                .catch(err => {
+                    console.log(err)
+                    Logger.Err("error occered during db connection");
+                })
             Logger.Imp(this.SERVER_STARTED + port);
         });
     }
