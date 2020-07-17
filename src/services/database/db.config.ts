4b78f7f40f48d3703f02565e07871b326a83da78
Pranav Bhat <pranava.sarang@tekizma.com>
2020-07-17 13:10:04 +0530

    added db config

--- src/services/database/db.config.ts
+++ src/services/database/db.config.ts
@@ -1,6 +1,6 @@
-import { Sequelize } from 'sequelize-typescript';
-
-export const sequelize = new Sequelize('d7u1gth52cks9o', 'gogninozxtxdjd', '0659fc0347e9d1c02b14af5c608c6eb9cd3cc78688dd53493eb914f3994842a5', {
+// import { Sequelize } from 'sequelize';
+var Sequelize = require('sequelize')
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: 'ec2-50-17-90-177.compute-1.amazonaws.com',
     dialect: 'postgres',
     port:5432,
